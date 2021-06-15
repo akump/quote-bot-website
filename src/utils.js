a48abddae9ed3df3883c65eb9577275caa747eea
Andrew Kump <kumpandrew@gmail.com>
2021-06-15 11:59:05 -0400

    refactored out capitalize

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,6 @@ const sleep = (milliseconds) => {
     return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
+const capitalize = (word) => word[0].toUpperCase() + word.slice(1);
 
-export { sleep };
+export { sleep, capitalize };
