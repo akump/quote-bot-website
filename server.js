268a50f117bc1a41e18fe5350f29c0c7534252e6
Andrew Kump <kumpandrew@gmail.com>
2021-06-15 00:35:42 -0400

    convert to module

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from 'express';
+import { resolve } from 'path';
 const app = express();
 const port = process.env.PORT || 5000;
-const path = require('path');
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile(resolve(__dirname, 'public', 'index.html'));
 });
 app.listen(port, () => {
     console.log(`Server is up at port ${port}`);
