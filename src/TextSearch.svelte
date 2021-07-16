cb4b5548881c251037ee3cfa04e1cd4b8b735d9e
Andrew Kump <kumpandrew@gmail.com>
2021-07-16 13:13:37 -0400

    wrap options in text search

--- src/TextSearch.svelte
+++ src/TextSearch.svelte
@@ -65,29 +65,33 @@
     });
 </script>
 
-<div class="label">Search type</div>
-{#each options as value}
-    <label
-        ><input
-            class="radio-button"
-            type="radio"
-            {value}
-            bind:group={selected}
-            on:click|once={handleRadio}
-        />{capitalize(value)}</label
-    >
-{/each}
-
-<div class="label">Show date quote was added</div>
-<Toggle
-    bind:dateToggle
-    on:click={setToggleLocalStorage}
-    bind:this={toggleElement}
-    hideLabel
-    toggledColor="#b10bb1"
-    label="Show date added?"
-/>
-
+<div class="options-container">
+    <div class="option-type">
+        <div class="label">Search type</div>
+        {#each options as value}
+            <label
+                ><input
+                    class="radio-button"
+                    type="radio"
+                    {value}
+                    bind:group={selected}
+                    on:click|once={handleRadio}
+                />{capitalize(value)}</label
+            >
+        {/each}
+    </div>
+    <div class="option-date">
+        <div class="label">Show date</div>
+        <Toggle
+            bind:dateToggle
+            on:click={setToggleLocalStorage}
+            bind:this={toggleElement}
+            hideLabel
+            toggledColor="#b10bb1"
+            label="Show date added?"
+        />
+    </div>
+</div>
 <form on:submit|preventDefault={findQuotes}>
     <label for="imageNum"
         >Find quotes: <input
@@ -144,5 +148,15 @@
 
     .radio-button {
         margin-right: 5px;
+    }
+    .options-container {
+        display: flex;
+        margin-bottom: 5px;
+    }
+    .option-type {
+        flex: 1;
+    }
+    .option-date {
+        flex: 1;
     }
 </style>
